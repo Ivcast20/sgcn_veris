429e5493ebae814af4d0ddfd94f91cd381261837
IvanCasa <ivanfrancisco_20@outlook.com>
2022-11-24 23:59:46 -0500

    cambios en user table

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -5,6 +5,9 @@ namespace App\Http\Livewire;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Rappasoft\LaravelLivewireTables\Views\Columns\BooleanColumn;
+use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 
 class UserTable extends DataTableComponent
 {
@@ -13,6 +16,15 @@ class UserTable extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id');
+        $this->setHideBulkActionsWhenEmptyEnabled();
+        $this->setFilterLayout('slide-down');
+    }
+
+    public function bulkActions(): array
+    {
+        return [
+            'exportSelected' => 'Export',
+        ];
     }
 
     public function columns(): array
@@ -20,14 +32,38 @@ class UserTable extends DataTableComponent
         return [
             Column::make("Id", "id")
                 ->sortable(),
-            Column::make("Name", "name")
+            Column::make(__('Name'), "name")
                 ->sortable(),
-            Column::make("Email", "email")
+            Column::make(__('Email'), "email")
                 ->sortable(),
-            Column::make("Created at", "created_at")
+            Column::make(__('Created at'), "created_at")
                 ->sortable(),
-            Column::make("Updated at", "updated_at")
+            Column::make(__('Updated at'), "updated_at")
                 ->sortable(),
+            BooleanColumn::make('Estado', 'status')
         ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            SelectFilter::make('Estado','status')
+                ->options([
+                    '' => 'Todos',
+                    '1' => 'Activo',
+                    '0' => 'Inactivo',
+                ])->filter(function(Builder $builder, string $value) {
+                    if ($value === '1') {
+                        $builder->where('status', true);
+                    } elseif ($value === '0') {
+                        $builder->where('status', false);
+                    }
+                }),
+        ];
+    }
+
+    public function exportSelected()
+    {
+        dd($this->getSelected());
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,7 +11,6 @@
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
