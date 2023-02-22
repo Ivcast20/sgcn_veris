757fc5a4da20e2c4129c7a4df747e7056d13d4ba
Ivan Castro <ivan20cast@gmail.com>
2023-02-22 18:01:30 -0500

    Cuase mcr

---
+++ app/Http/Controllers/CauseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cause;
+use Illuminate\Http\Request;
+
+class CauseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Cause  $cause
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cause $cause)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Cause  $cause
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cause $cause)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Cause  $cause
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cause $cause)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Cause  $cause
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cause $cause)
+    {
+        //
+    }
+}
