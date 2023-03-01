62b828217f1622751753b591f401148807118126
Adriana Hidalgo <ivanfrancisco_20@outlook.com>
2023-03-01 02:11:39 -0500

    Status Risks modelo -mcr

---
+++ app/Http/Controllers/StatusRiskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StatusRisk;
+use Illuminate\Http\Request;
+
+class StatusRiskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\StatusRisk  $statusRisk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StatusRisk $statusRisk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\StatusRisk  $statusRisk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StatusRisk $statusRisk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\StatusRisk  $statusRisk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StatusRisk $statusRisk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\StatusRisk  $statusRisk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StatusRisk $statusRisk)
+    {
+        //
+    }
+}
