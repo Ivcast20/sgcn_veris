8bd9df54a7c1018603d0e01505882a3cbcf106d4
Adriana Hidalgo <ivanfrancisco_20@outlook.com>
2023-03-01 02:12:08 -0500

    Modelo Risk -mcr

---
+++ app/Models/Risk.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Risk extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_03_01_012311_create_risks_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('risks', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',50)->unique();
+            $table->string('description',1000);
+            $table->text('consecuences');
+            $table->foreignId('risk_owner_id')->constrained('departments');
+            $table->string('existing_controls',1000);
+            $table->integer('probability');
+            $table->integer('impact');
+            $table->integer('risk_level');
+            $table->boolean('is_aceptable');
+            $table->foreignId('treatment_option_id')->constrained('treatment_options');
+            $table->text('treatment_plan')->nullable();
+            $table->string('responsable',500)->nullable();
+            $table->string('resources',500)->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->foreignId('status_id')->nullable()->constrained('status_risks');
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('risks');
+    }
+};
